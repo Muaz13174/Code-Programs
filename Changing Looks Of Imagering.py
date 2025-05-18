6c11568a153400f942f099428dc854544f70351c
DonaRGB <thongdadona@gmail.com>
2025-05-18 16:42:29 +0700

    Initial commit

--- Changing Looks Of Imagering.py
+++ Changing Looks Of Imagering.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-img = cv2.imread("boy.jpeg")
+img = cv2.imread("test pic.jpg")
 imgrgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 plt.imshow(imgrgb)
 plt.title("RGB Image")
@@ -10,7 +10,7 @@ gimg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 plt.imshow(gimg,cmap = "gray")
 plt.title("Grayscale Image")
 plt.show()
-cimg = img[50:45,100:10]
+cimg = imgrgb[450:700,400:700]
 crgb = cv2.cvtColor(cimg,cv2.COLOR_BGR2RGB)
 plt.imshow(crgb)
 plt.title("Cropped Region")
