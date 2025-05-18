908376a7431849a4159c39975fe64fb35bd034c0
DonaRGB <thongdadona@gmail.com>
2025-05-18 08:39:04 +0700

    Initial commit

--- Image Resizerator.py
+++ Image Resizerator.py
@@ -1,20 +1,23 @@
 import cv2
 from time import sleep as slp
 img = cv2.imread("test pic.jpg")
-rimg = cv2.resize(img,(300,300))
 cv2.imshow(str(img.shape),img)
 if cv2.waitKey(0) == 27:
     cv2.destroyAllWindows()
-while True:
-    size = str(input("\nWhat size do you want the image to be? (small/medium/large): ")).lower()
-    if size == "s" or size == "small":
-        rimg = cv2.resize(img,(200,200))
-    elif size == "m" or size == "medium":
-        rimg = cv2.resize(img,(400,400))
-    elif size == "l" or size == "large":
-        rimg = cv2.resize(img,(600,600))
-    else:
-        print("Sorry, no such size.")
-        continue
-    break
-cv2.imshow(rimg)
+def imgs():
+    while True:
+        rimg = cv2.resize(img,(300,300))
+        size = str(input("\nWhat size do you want the image to be? (small/medium/large): ")).lower()
+        if size == "s" or size == "small":
+            rimg = cv2.resize(img,(200,200))
+            break
+        elif size == "m" or size == "medium":
+            rimg = cv2.resize(img,(400,400))
+            break
+        elif size == "l" or size == "large":
+            rimg = cv2.resize(img,(600,600))
+            break
+        else:
+            print("Sorry, no such size.")
+            continue
+
